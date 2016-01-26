fa28e5f700222ef2aeb1475678af233995c0de8f
Carlos Hernandez <carlos@techbyte.ca>
2016-01-26 14:54:37 -0700

    Macros working with

--- mutt/root/usr/lib/muttbot/accounts.py
+++ mutt/root/usr/lib/muttbot/accounts.py
@@ -94,7 +94,7 @@ class AccountsSetuper(object):
             f.write('set header_cache = ~/.mutt/cache/' + account_name +'/headers\n')
             f.write('set message_cachedir = ~/.mutt/cache/' + account_name +'/bodies\n')
             f.write('set certificate_file = ~/.mutt/cache/' + account_name +'/certificates\n')
-            f.write('set account_tag = ' + account_name + '\n')
+            f.write('set my_account_tag = ' + account_name + '\n')
             f.write('source ~/.mutt/muttrc.folder.bindings\n')
             f.close
 
