bc601d45f4d36e0bdbe6f6bdd5059a90dcb837a5
Carlos Hernandez <carlos@techbyte.ca>
2016-04-14 16:29:16 -0600

    Fixing file notifications for mutt.

--- mutt/root/usr/local/bin/email_notifier.py
+++ mutt/root/usr/local/bin/email_notifier.py
@@ -10,12 +10,16 @@ from email.header import decode_header
 maildir = os.path.join('/home/' + os.environ.get('APP_USER'), '.mail')
 # Get email accounts
 accounts = filter( lambda f: not f.startswith('.'), os.listdir(maildir))
-notifciation_dir = os.path.join('/home/' + os.environ.get('APP_USER'), '/.notifications')
+notification_dir = os.path.join('/home/' + os.environ.get('APP_USER'), '.notifications')
 if not os.path.exists(notification_dir):
     os.makedirs(notification_dir)
-fnotify = os.path.join('/home/' + os.environ.get('APP_USER'), '/.notifications/email')
+fnotify = os.path.join(notification_dir, 'email')
 
-pynotify.init(r'email_notifier.py')
+if not os.path.exists(fnotify):
+    open(fnotify, 'a').close()
+
+# email decoding
+dec_header = lambda h : ' '.join(unicode(s, e if bool(e) else 'ascii') for s, e in decode_header(h))
 
 def newfile(event):
     fd = open(event.pathname, 'r')
@@ -36,6 +40,6 @@ notifier = pyinotify.Notifier(wm, newfile)
 for account in accounts:
     boxes = os.listdir(os.path.join(maildir, account))
     for box in boxes:
-        wm.add_watch(os.path.join(maildir,account,box,new), pyinotify.IN_CREATE | pyinotify.IN_MOVED_TO)
+        wm.add_watch(os.path.join(maildir,account,box,'new'), pyinotify.IN_CREATE | pyinotify.IN_MOVED_TO)
 
 notifier.loop()
